710d25a66b1b69485fe5a5a94e7e104385617652
Faiq Talat <faiktalat@gmail.com>
2016-01-03 02:48:28 -0800

    Initial Commit

---
+++ iHoursTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iHoursTracker
+//
+//  Created by Rehanuddin on 03/01/2016.
+//  Copyright (c) 2016 Faiq Talat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
